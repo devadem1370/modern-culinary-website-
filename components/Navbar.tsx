1ef9a92b26b789c9eda8048d5da012ab4b8dd082
Adil Imran <devadem1370@gmail.com>
2025-05-29 17:35:44 -0700

    create navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import React from "react";
+
+const navItems = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "About",
+    href: "/",
+  },
+  {
+    label: "Foods",
+    href: "/",
+  },
+];
+const Navbar = () => {
+  return (
+    <nav className=" max-w-[1250px] mt-[40px] mx-auto flex justify-between items-center">
+      <div className="logo flex items-center justify-center gap-3">
+        <Image src="/icons/logo.svg" alt="logo" width={42} height={42} />
+        <span>Savory</span>
+      </div>
+
+
+      <div className="flex items-center gap-10 ">
+        {navItems.map((item) => (
+          <a href={item.href} key={item.label}>
+            {item.label}
+          </a>
+        ))}
+      </div>
+      <div className="search flex items-center justify-between outline outline-white/40 rounded-full py-[8px] px-8 w-[398px]">
+        <input type="text" placeholder="Search..." className="outline-none bg-transparent"/>
+        <Image src="/icons/search.svg" alt="search" width={31} height={31}/>
+      </div>
+      <div className="icons">
+        <Image src="/icons/icons.png" alt="user" width={31} height={31}/>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
